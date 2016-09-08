e3081f2ec281149409d1b027205e4b473f3d1324
jooshint <jooshint@greeny.cs.tlu.ee>
2016-09-08 13:11:24 +0300

    Added my firs line of commands

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 JOOSEP
+
+Minu kassi nimi on Kiku!
---
+++ first_file.php
@@ -0,0 +1,13 @@
+<?php
+
+    $firstName = "Joosep";
+	$lastName = "Hint";
+
+	
+	//echo $firstName;
+	//echo $lastName;
+	
+	//Kahe stringi liitmiseks kasutan  punkti
+	echo $firstName." ".$lastName;
+?>
+
